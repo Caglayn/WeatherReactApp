7e55abb66fe2f5e33d0f21eed7c004d35d9a0c3f
caglayn <caglayankaya@hotmail.com.tr>
2024-02-27 13:13:57 +0300

    fix

--- src/api/RestApiUrls.js
+++ src/api/RestApiUrls.js
@@ -1,5 +1,5 @@
-export const USER_SERVICE = 'http://reactiveUserService:8081/api/v1/user';
-export const WEATHER_SERVICE = 'http://weatherService:8082/api/v1/weather';
+export const USER_SERVICE = 'http://89.117.48.141:8081/api/v1/user';
+export const WEATHER_SERVICE = 'http://89.117.48.141:8082/api/v1/weather';
 
 export const LOGIN = '/login';
 export const GET_INFO = '/getinfo';
